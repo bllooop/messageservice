b5eb68a48bcb3066b9bcbf5058ca26a65a6cfc30
Diana <dianaartn@mail.ru>
2024-07-31 03:20:01 +0400

    fix

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package messageservice
 import (
 	"context"
 	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -11,8 +12,11 @@ type Server struct {
 
 func (s *Server) RunServer(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
-		Addr:    ":" + port,
-		Handler: handler,
+		Addr:         ":" + port,
+		Handler:      handler,
+		ReadTimeout:  5 * time.Minute,
+		WriteTimeout: 5 * time.Minute,
+		IdleTimeout:  5 * time.Minute,
 	}
 	return s.httpServer.ListenAndServe()
 }
