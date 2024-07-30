2bfcde1b9331d443b52d3d64916dcc19674171db
Diana <dianaartn@mail.ru>
2024-07-30 17:32:07 +0400

    fix

--- pkg/messaging/consumer.go
+++ pkg/messaging/consumer.go
@@ -1,6 +1,7 @@
 package messaging
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -17,6 +18,7 @@ type KafkaConsumer struct {
 }
 
 func (kc *KafkaConsumer) ConsumeKafka() error {
+
 	var err error
 	kc.c, err = kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "kafka:9092",
@@ -38,30 +40,8 @@ func (kc *KafkaConsumer) ConsumeKafka() error {
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
 	/*go func() {
-		for {
-			ev := kc.c.Poll(100)
-			switch e := ev.(type) {
-			case *kafka.Message:
-				av, err := kc.c.ReadMessage(100 * time.Millisecond)
-				if err != nil {
-					continue
-				}
-				fmt.Printf("Consumed event from topic %s: text = %s\n, time = %s\n",
-					*av.TopicPartition.Topic, string(av.Value), av.Timestamp)
-			case kafka.Error:
-				fmt.Printf("Error: %v\n", e)
-			case *kafka.Stats:
-				stats := map[string]interface{}{}
-				if err := json.Unmarshal([]byte(e.String()), &stats); err != nil {
-					log.Printf("Failed to parse statistics: %v\n", err)
-				} else {
-					log.Printf("Consumer statistics: %v\n", stats)
-				}
-			}
-		}
-	}()
-	go func() {
 		for {
 			ev, err := kc.c.ReadMessage(80 * time.Millisecond)
 			if err != nil {
@@ -78,8 +58,20 @@ func (kc *KafkaConsumer) ConsumeKafka() error {
 			switch e := ev.(type) {
 			case *kafka.Message:
 				prometheus.KafkaMessages.WithLabelValues(*e.TopicPartition.Topic).Inc()
-				fmt.Printf("Consumed event from topic %s: text = %s\n, time = %s\n",
-					*e.TopicPartition.Topic, string(e.Value), e.Timestamp)
+				//fmt.Printf("Consumed event from topic %s: text = %s\n, time = %s\n",
+				//*e.TopicPartition.Topic, string(e.Value), e.Timestamp)
+				messageData := map[string]interface{}{
+					"topic":     *e.TopicPartition.Topic,
+					"text":      string(e.Value),
+					"timestamp": e.Timestamp,
+				}
+				jsonData, err := json.MarshalIndent(messageData, "", "  ")
+				if err != nil {
+					log.Printf("Error marshaling JSON: %s", err)
+					continue
+				}
+				fmt.Println("Consumed event " + string(jsonData))
+
 			/*case kafka.Error:
 			prometheus.KafkaErrors.WithLabelValues(*e.Code().String()).Inc()
 			fmt.Printf("Error: %v\n", e) */
